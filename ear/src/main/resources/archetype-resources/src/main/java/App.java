2f7cc72dd70aef050f080165082245cac8aa49c0
Lucas <lamantovani@gmail.com>
2017-11-15 14:57:28 -0200

    Commit inical 2

---
+++ ear/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.lucascorp.traking.server.ear;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ service/src/main/java/com/lucascorp/traking/server/service/App.java
@@ -0,0 +1,13 @@
+package com.lucascorp.traking.server.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
